6136fdf698ef47921e855d72ff73bee3557bad7a
Bautegg <97198351+Bautegg@users.noreply.github.com>
2023-09-27 13:22:05 +0200

    code cleaning

--- collatz_analyze.py
+++ collatz_analyze.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly.express as px
-import numpy as np
+
 
 def load_parquet_data(path):
     df = pd.read_parquet(path)
@@ -13,34 +12,17 @@ def intro_analyze(path):
     pd.set_option('display.float_format', lambda x: '%.4f' % x)
     df = df.assign(Steps_by_number=df['Steps_y'] / df['Number_x'])
     df2 = df.query('Steps_y == 0 or `Number_x` == 0')
-    # print(df2)
-    # df2 = df.drop([0, 1])
-    # df2 = df2.assign(steps_by_number=df['Number (x)'] / ['Steps (y)'])
-    # print('df2: ', df2.head())
+    print(df2)
     print(df.info())
     df_head = df.head(30)
-    # df_head_ass = df_head.assign(steps_by_number=df_head['Steps (y)']/df_head['Number (x)'])
     df_tail = df.tail()
     df_describe = df.describe()
-    # df_min = df.min()
-    # df_max = df.max()
-
-    df_count_155 = df[df.Steps_y == 120].count()
-    df_count = df.Steps_y.value_counts()
-    # df_count_155 = df[df.steps_by_number == 0.5].count()
-    # df_count_155 = df[df.Steps_y == 155].count()
-
 
     print(
           f'\033[1m*|* Summary of analyze: \033[0m\n\n '
           f'** Head is: \n{df_head} **\n '
-          f'** 155 steps occurs: \n{df_count_155} **\n '
-          f'** Count of steps is: \n{df_count} **\n '
-          # f'** Head with Steps / Nmber: \n{df_head_ass} **\n '
           f'** Tail is: \n{df_tail} **\n '
-          # f'** Describe is: \n{df_describe} **\n '
-          # f'** Minimal Value is: \n{df_min} **\n '
-          # f'** Maximal Value is: \n{df_max} **\n\n'
+          f'** Describe is: \n{df_describe} **\n '
     )
 
     return df
@@ -51,7 +33,7 @@ def draw_plot(path):
     plot_df = load_parquet_data(path)
     plot_df_ratio = intro_analyze(path)
     # assign specific series of Data Frame to variables
-    x = plot_df.loc[:, 'Number_x'] # is it possible it's more optimasie than df['Number_x'] in  ax2.scatter
+    x = plot_df.loc[:, 'Number_x']
     y = plot_df.loc[:, 'Steps_y']
     ratio = plot_df_ratio.loc[:, 'Steps_by_number']
     # draw y(x) plot
@@ -86,6 +68,5 @@ def draw_histogram(path):
 
 if __name__ == '__main__':
     DATA_PATH = "collatz_data_20kk_v2-gz.parquet"
-    print(f' ** End of Collatz Problem analyze: {intro_analyze(DATA_PATH)} ** \n')
     print(f' ** Draw plot: {draw_plot(DATA_PATH)}')
     print(f' ** Draw histogram: {draw_histogram(DATA_PATH)}')
